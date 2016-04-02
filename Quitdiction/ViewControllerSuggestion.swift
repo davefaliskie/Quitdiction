1889a9421a712580d2d52e540f8d37bfb42f2231
Dave Faliskie <davefaliskie@gmail.com>
2016-04-02 02:04:24 -0400

    added suggestions page

---
+++ Quitdiction/ViewControllerSuggestion.swift
@@ -0,0 +1,44 @@
+//
+//  ViewControllerSuggestion.swift
+//  Quitdiction
+//
+//  Created by David Faliskie on 4/2/16.
+//  Copyright © 2016 David Faliskie. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerSuggestion: UIViewController {
+    
+    @IBOutlet weak var suggestionLabel: UILabel!
+    @IBAction func noWayButton(sender: AnyObject) {
+        generateSuggestion()
+    }
+    
+    
+   
+    // all the suggestions
+    var suggestionData = [
+        "Go to bed",
+        "Cry",
+        "Fight someone"
+    ]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    func generateSuggestion() {
+        let randomIndex = Int(arc4random_uniform(UInt32(suggestionData.count)))
+        suggestionLabel.text = suggestionData[randomIndex]
+
+    }
+}
+
