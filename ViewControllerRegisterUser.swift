abdb947ecda111a2c4b5ff8aba4ffe33f0280360
Riley Sung <12sungr@gmail.com>
2016-04-02 10:53:17 -0400

    Register User Start

--- ViewControllerRegisterUser.swift
+++ ViewControllerRegisterUser.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewControllerRegisterUser: UIViewController {
     
+    var firstName = String()
+    var LastName = String()
+    var userName = String()
+    var selectedGender = String()
+    var birthday = NSDate()
+    
+    @IBOutlet var firstNameTextField: UITextField!
+    @IBOutlet var lastNameTextField: UITextField!
+    @IBOutlet var userNameTextField: UITextField!
+  
+    @IBOutlet var genderSegmentControl: UISegmentedControl!
+    @IBAction func genderSegmentControlIndexChanged(sender: AnyObject) {
+        switch genderSegmentControl.selectedSegmentIndex {
+            case 0:
+                selectedGender = "Male"
+            case 1:
+                selectedGender = "Female"
+            default:
+                break;
+        }
+        
+    }
+    
+   
+    @IBOutlet var birthdayDatePicker: UIDatePicker!
+    
+    @IBAction func registerButton(sender: AnyObject) {
+        registerUser()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +49,10 @@ class ViewControllerRegisterUser: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func registerUser() {
+        
     }
 
 
